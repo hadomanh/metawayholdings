094a8dca8c609cbca5b1acbed4d247ccb5f4442a
hadomanh <hadomanh99@gmail.com>
2022-02-09 14:11:29 +0700

    Update https

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,6 +8,9 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+
   <title>METAWAYCHAIN Admin</title>
 
   <!-- Google Font: Source Sans Pro -->
