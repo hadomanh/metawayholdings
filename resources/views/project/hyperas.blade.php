a9104dd2eb59ab12ce79a839c3fdc635de799c52
mannghi <mannghi@gmail.com>
2022-03-08 16:12:18 +0700

    add read more

--- resources/views/project/hyperas.blade.php
+++ resources/views/project/hyperas.blade.php
@@ -22,17 +22,18 @@
                 <img src="{{ asset('img/project-overview.png') }}" alt="HYPERAS CHAIN overview">
             </div>
             <div class="col-md-6">
-                <h2 class="titleUnderline">OVERVIEW</h2>
-                <div class="text-justufy">
-                    <p>Hyperas Chain (Hyra coin) is created by HYPERAS TECHNOLOGIES INC. </p>
-                    <p>Hyperas chain is a blockchain infrastructure that helps connect other blockchains.</p>
-                    <p>With our non-invasive cross-chain technology Hyperas chain operates as a data conduit, allowing cross-chain communications between public and private protocols. </p>
-                    <p>Hyperas chain can transfer both assets and data without requiring any change on participating blockchains.</p>
-                    <p>Our unified infrastructure helps solutions run on multiple platforms by enabling interoperability among them and making the implementation as straightforward as possible from the developer's perspective.</p>
-                    <p>The founders of Hyperas Chain seek to cooperate with strategic partners, product developers, venture capital funds, investors, and global users, who lend their hands in developing this platform to bring real values to the global community fans that love blockchain technology and digital assets.</p>
+                    <h2 class="titleUnderline">OVERVIEW</h2>
+                    <div class="text-justufy">
+                        <p>Hyperas Chain (Hyra coin) is created by Hyperas INC. </p>
+                        <p>Hyperas chain is a blockchain infrastructure that helps connect other blockchains.</p>
+                        <p>With our non-invasive cross-chain technology Hyperas chain operates as a data conduit, allowing cross-chain communications between public and private protocols. </p>
+                        <p>Hyperas chain can transfer both assets and data without requiring any change on participating blockchains.</p>
+                        <p>Our unified infrastructure helps solutions run on multiple platforms by enabling interoperability among them and making the implementation as straightforward as possible from the developer's perspective.</p>
+                        <p>The founders of Hyperas Chain seek to cooperate with strategic partners, product developers, venture capital funds, investors, and global users, who lend their hands in developing this platform to bring real values to the global community fans that love blockchain technology and digital assets.</p>
+                    </div>
+                    <div class="text-center"><a target="_blank" href="https://hyperaschain.com/" class="btn btnWhite">READ MORE</a></div>
 
                 </div>
-            </div>
         </div>
     </section>
 
