2dc17380a8b09e9d04b6110486dba448a218877b
Ha Do Manh <hadomanh@MacBook-Pro-cua-Ha.local>
2022-03-16 20:13:30 +0700

    Update Google tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -257,6 +257,17 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> --}}
 	<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CCGS9ZB4JL"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-CCGS9ZB4JL');
+	</script>
+
 	@stack('script')
 </body>
 
