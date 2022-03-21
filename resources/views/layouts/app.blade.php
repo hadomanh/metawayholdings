9368870c84073ca99c96a90366aeed22a0b3dd68
Ha Do Manh <52105195+hadomanh@users.noreply.github.com>
2022-03-21 15:38:55 +0700

    Remote Google tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,16 +2,6 @@
 <html lang="en">
 
 <head>
-
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CCGS9ZB4JL"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', 'G-CCGS9ZB4JL');
-	</script>
 	
 	<meta charset="utf-8">
 	<!-- Main Tag -->
